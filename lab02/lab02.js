d0a77215761e40ecebb2a924e4cf4bdca154e2b2
SnarkosaurusRex <lydiaholtrop@gmail.com>
2018-09-13 23:10:36 -0400

    All done :)

--- lab02/lab02.js
+++ lab02/lab02.js
@@ -81,6 +81,69 @@ console.log("----------------------------------------------");
 
 // Test greeting
 dude2.greet();
+console.log("----------------------------------------------");
 
 
+// Exercise 2.2 ----------------------------------
+// Inheritance and Polymorphism
+function Student(name, birthdate, friends, major) {
+    Person.call(this, name, birthdate, friends);
+    this.major = major;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.changeMajor = function(newMajor) {
+    this.major = newMajor;
+};
+
+Student.prototype.greet = function() {
+	console.log("I am a school-going hooman (a.k.a. a student)");
+};
+
+
+// Tests
+// Test student stuff
+var kid1 = new Student("Bob the Tomato", "12-21-1993", [], "Agriculture");
+console.log(kid1);
+var kid2 = new Student("Luna Lovegood", "06-21-2003", [], "Classics");
+console.log(kid2);
+var isInstance = kid1 instanceof Person;
+console.log("\n" + kid1.name + " is a Person --> " + isInstance);
+isInstance = kid1 instanceof Student;
+console.log(kid1.name + " is a Student --> " + isInstance);
+isInstance = dude1 instanceof Person;
+console.log(dude1.name + " is a Person --> " + isInstance);
+isInstance = dude1 instanceof Student;
+console.log(dude1.name + " is a Student --> " + isInstance);
+//console.log(kid1 instanceof Person);
+console.log("----------------------------------------------");
+
+// Test changing name & major
+kid1.changeName("Larry the Cucumber");
+kid1.changeMajor("Biology");
+console.log(kid1);
+console.log("----------------------------------------------");
+
+
+// Test getting age
+console.log(kid1.getAge());
+console.log(kid2.getAge());
+console.log("----------------------------------------------");
+
+
+// Test adding friends
+kid1.addFriend(kid2);
+kid1.addFriend(dude1);
+kid1.addFriend(dude2);
+dude1.addFriend(kid2);
+console.log(kid1);
+console.log(dude1);
+console.log("----------------------------------------------");
+
+
+// Test greeting
+dude4.greet();
+kid1.greet();
+console.log("----------------------------------------------");
 
